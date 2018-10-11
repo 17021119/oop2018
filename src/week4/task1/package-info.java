77bbb3c8cc1fcddd5bdb7d824db1b0d542260afa
17021119 <ituet1119@gmail.com>
2018-10-11 21:59:52 +0700

    push

--- src/week4/task1/package-info.java
+++ src/week4/task1/package-info.java
@@ -1,9 +1,147 @@
-/**
- * Package bài làm cho Câu 1
- * TODO: Khai báo các lớp (class) theo Câu 1 yêu cầu. 1 file chỉ khai báo 1 class
- * TODO: Mô tả các quan hệ giữa các class và thêm thuộc tính, phương thức cho chúng (mỗi class ít nhất 2 phương thức, 2 thuộc tính)
- * TODO: Viết comment đầy đủ cho class, phương thức. Xem hướng dẫn tại đây (http://dongdiemthuy.github.io/java/javadoc/2014/09/30/su-dung-javadoc-va-maven-de-tao-tai-lieu-du-an.html)
- * @author cuong
- * @version 0.1
- */
 package week4.task1;
+
+import java.util.Date;
+
+public class HoaQua {
+    private String ten;
+    private int giaban;
+    private String nguongoc;
+    private String noisanxuat;
+
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public int getGiaban() {
+        return giaban;
+    }
+
+    public void setGiaban(int giaban) {
+        this.giaban = giaban;
+    }
+
+    public String getNguongoc() {
+        return nguongoc;
+    }
+
+    public void setNguongoc(String nguongoc) {
+        this.nguongoc = nguongoc;
+    }
+
+    public String getNoisanxuat() {
+        return noisanxuat;
+    }
+
+    public void setNoisanxuat(String noisanxuat) {
+        this.noisanxuat = noisanxuat;
+    }
+    public void HoaQua(String ten,int giaban,String nguongoc,String noisanxuat){
+        this.ten=ten;
+        this.giaban=giaban;
+        this.nguongoc=nguongoc;
+        this.noisanxuat=noisanxuat;
+    }
+    public void printHoaQua(){
+        System.out.println("Tên loại quả: "+this.ten);
+        System.out.println("Giá bán: "+this.giaban+" VNĐ/kg");
+        System.out.println("Nguồn gốc: "+this.nguongoc);
+        System.out.println("Nơi sản Xuất: "+this.noisanxuat);
+    }
+}
+class Cam extends HoaQua{
+    private String color;
+    private int soluong;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+    public void Cam(String color,int soluong){
+        this.color=color;
+        this.soluong=soluong;
+    }
+    public void printfCam(){
+        System.out.println("Trung bình khoảng: "+this.soluong+" quả/kg");
+        System.out.println("Màu vỏ: "+this.color);
+    }
+}
+class Tao extends HoaQua{
+    private int trongluongTB;
+    private String loaitao;
+
+    public int getTrongluongTB() {
+        return trongluongTB;
+    }
+
+    public void setTrongluongTB(int trongluongTB) {
+        this.trongluongTB = trongluongTB;
+    }
+
+    public String getLoaitao() {
+        return loaitao;
+    }
+
+    public void setLoaitao(String loaitao) {
+        this.loaitao = loaitao;
+    }
+    public void Tao(int trongluongTB,String loaitao){
+        this.trongluongTB=trongluongTB;
+        this.loaitao=loaitao;
+    }
+    public void printfTao(){
+        System.out.println("Trọng lượng trung bình: "+this.trongluongTB+" gam/quả");
+        System.out.println("Loại táo: "+this.loaitao);
+    }
+}
+class CamThanhPhong extends Cam{
+    private String vicam;
+
+    public String getVicam() {
+        return vicam;
+    }
+
+    public void setVicam(String vicam) {
+        this.vicam = vicam;
+    }
+    public void CamThanhPhong(String vicam){
+        this.vicam=vicam;
+    }
+    public void printfCamThanhPhong(){
+        System.out.println("Cam "+this.vicam);
+    }
+}
+class CamSanh extends CamThanhPhong{
+    public static void main(String[] args) {
+        CamSanh cs=new CamSanh();
+        cs.CamThanhPhong("chua");
+        cs.Cam("màu xanh lá cây đậm", 3);
+        cs.HoaQua("Cam Sành",20000, "Thanh Phong, Thành phố Phủ Lý, Hà Nam", "Công ty Cam Sành Thanh Phong");
+        cs.printHoaQua();
+        cs.printfCam();
+        cs.printfCamThanhPhong();
+        System.out.println("===========================================================");
+        Tao t=new Tao();
+        t.Tao(300, "Táo ta");
+        t.HoaQua("Táo",30000, "Duyên Hải, Hưng Hà, Thái Bình", "Công ty TNHH Kết Thành");
+        t.printHoaQua();
+        t.printfTao();
+
+    }
+}
+
--- src/week4/task2/package-info.java
+++ src/week4/task2/package-info.java
@@ -1,9 +1,167 @@
-/**
- * Package bài làm cho Câu 2
- * TODO: Khai báo các class, thuộc tính và phương thức như biểu đồ lớp của Câu 2. 1 file chỉ khai báo 1 class
- * TODO: Viết comment và khai báo getter, setter đầy đủ như yêu cầu ở Câu 1
- * TODO: Sử dụng số PI tự định nghĩa dùng cho các phương thức getArea() và getPerimeter()
- * @author cuong
- * @version 0.1
- */
 package week4.task2;
+public class Shape {
+    private String color;
+    private boolean filled;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    public Shape(){
+        // khởi tạo thông tin mặc định cho Shape
+        this.color="red";
+        this.filled=true;
+    }
+    public Shape(String color,boolean filled){
+        this.color=color;
+        this.filled=filled;
+    }
+    public String toString(){
+        // biểu diễn đối tượng Shape dưới dạng chuỗi
+        return this.getColor()+" "+this.filled;
+    }
+}
+class Circle extends Shape{
+    final double PI=3.14;
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    Circle(){
+        this.radius=1.0;
+    }
+    Circle(double radius){
+        this.radius=radius;
+    }
+    Circle(double radius,String color,boolean filled){
+        this.radius=radius;
+        this.setColor(color);
+        this.setFilled(filled);
+    }
+    public double getArea(){
+        //Diện tích hình tròn
+        return this.radius*this.radius*PI;
+    }
+    public double getPerimeter(){
+        //chu vi hình tròn
+        return 2*PI*this.radius;
+    }
+    public String toString(){
+        return this.radius+" "+this.getArea()+" "+this.getPerimeter()+" "+this.getColor()+" "+this.isFilled();
+    }
+
+}
+class Rectangle extends Shape{
+    private double width;
+    private double length;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    Rectangle() {
+        this.length=1.0;
+        this.width=1.0;
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        //kế thừa từ lớp cha Shape
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+    public double getArea(){
+        return this.length*this.width;
+    }
+    public double getPerimeter(){
+        return 2*(this.length+this.width);
+    }
+    public String toString(){
+        return this.width+" "+this.length+" "+this.getArea()+" "+this.getPerimeter()+
+                " "+this.getColor()+" "+this.isFilled();
+    }
+}
+class Square extends Rectangle{
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+    public Square() {
+        this.side=this.getLength();
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+//    Square(double side,String color,boolean filled){
+//        this.setColor(color);
+//        this.setFilled(filled);
+//        this.side=side;
+//    }
+//    public void setWidth(double side){
+//        this.setWidth(side);
+//    }
+//    public void setLength(double side){
+//        this.setLength(side);
+//    }
+
+    public Square(double side,String color, boolean filled){
+        super(side, side, color, filled);
+        this.side = side;
+    }
+
+    public String toString() {
+        return this.side+" "+this.getArea()+" "+this.getPerimeter()+" "+this.getColor()
+                +" "+this.isFilled();
+    }
+    public static void main(String[] args) {
+        System.out.println("Hình tròn");
+        Circle c=new Circle(3.8, "blue", true);
+        System.out.println(c.toString());
+        System.out.println("Hình Vuông");
+        Square s=new Square(1.2);
+        s=new Square(1.2, "red", false);
+        System.out.println(s.toString());
+
+    }
+}
